fac25646a7ba2040c839442e510eeecae001bb0a
jhondoeelmisterio <69824622+jhondoeelmisterio@users.noreply.github.com>
2021-01-17 04:39:53 +0000

    Update and rename README.md to traductor.py

---
+++ traductor.py
@@ -0,0 +1,399 @@
+#Created with much love by Jhon Doe
+
+#Importes
+
+import os
+import sys
+
+#Bienvenida
+
+os.system("sleep 2")
+
+print ("                    >>>>>>>>>>>>>>>")
+print ("                       Bienvenido  ")
+print ("                    <<<<<<<<<<<<<<<")
+
+os.system("sleep 2")
+
+print ("")
+
+print ("Este script nos brinda un encriptador y decifrador del idioma lenur (creado por mi) y una recreacion manual del root")
+
+print ("")
+
+print ("Abriendo menu....")
+
+os.system("sleep 2")
+
+#Interfaz
+
+print ("")
+print ("                    =°=°=°=°=°=°=°=")
+print ("                          MENU     ")
+print ("                    =°=°=°=°=°=°=°=")
+
+os.system("sleep 2")
+
+print ("1) Lenur")
+print ("2) Root")
+
+os.system("sleep 2")
+
+print ("Que opción desea elegir?")
+eleccion = int(input(""))
+
+                         #SINTAXIS DE LENUR
+os.system("clear")
+
+os.system("sleep 2")
+
+if eleccion ==1:
+        print ("                    πππππππππππππππ")
+        print ("                         LENUR     ")
+        print ("                    πππππππππππππππ")
+
+os.system("sleep 1")
+
+print ("1)Encriptar")
+print ("2)Desencriptar")
+eleccion2 = int(input(""))
+
+#Encriptacion
+
+os.system("sleep 1")
+
+if eleccion2 ==1:
+        print ("Elija cuantas letras tiene su palabra")
+        print ("                                            Solo admite")
+        print ("                                            hasta 8 letras")
+        eleccion3 = int(input(""))
+
+os.system("sleep 1")
+
+#Cuatro letras
+
+
+if eleccion3 ==2:
+        letra1 = str(input("Ponga la primera letra >> "))
+        letra2 = str(input("Ponga la segunda letra >> "))
+        letra3 = str(input("Ponga la tercera letra >> "))
+        letra4 = str(input("Ponga la cuarta letra >> "))
+
+
+
+print ("##################################")
+print("")
+
+print ("palabra encriptada")
+
+if letra1 =="a":
+        a = "a"
+        print (a)
+elif letra1 =="b":
+        b = "n"
+        print (b)
+elif letra1 =="c":
+        c = "v"
+        print (c)
+elif letra1 =="d":
+        d = "c"
+        print (d)
+elif letra1 =="e":
+        e = "3"
+        print (e)
+elif letra1 =="f":
+        f = "d"
+        print (f)
+elif letra1 =="g":
+        g = "e"
+        print (g)
+elif letra1 =="h":
+        h = "f"
+        print (h)
+elif letra1 =="i":
+        i = "8"
+        print (i)
+elif letra1 =="j":
+        j = "g"
+
+elif letra1 =="k":
+        k = "h"
+        print (k)
+elif letra1 =="m":
+        m = "m"
+        print (m)
+elif letra1 =="n":
+        n = "m"
+        print (n)
+elif letra1 =="l":
+        l = "i"
+        print (l)
+elif letra1 =="o":
+        o = "9"
+        print (o)
+elif letra1 =="p":
+        p = "0"
+        print (p)
+elif letra1 =="q":
+        q = "1"
+        print (q)
+elif letra1 =="r":
+        r = "4"
+        print (r)
+elif letra1 =="s":
+        s = "b"
+        print (s)
+elif letra1 =="t":
+        t = "5"
+        print (t)
+elif letra1 =="u":
+        u = "7"
+        print (u)
+elif letra1 =="v":
+        v = "b5"
+        print (v)
+elif letra1 =="w":
+        w = "2"
+        print (w)
+elif letra1 =="x":
+        x = "c3"
+        print (x)
+elif letra1 =="y":
+        y = "6"
+        print (y)
+elif letra1 =="z":
+        z = "x1"
+
+
+
+if letra2 =="a":
+        a = "a"
+        print (a)
+elif letra2 =="b":
+        b = "n"
+        print (b)
+elif letra2 =="c":
+        c = "v"
+        print (c)
+elif letra2 =="d":
+        d = "c"
+        print (d)
+elif letra2 =="e":
+        e = "3"
+        print (e)
+elif letra2 =="f":
+        f = "d"
+        print (f)
+elif letra2 =="g":
+        g = "e"
+        print (g)
+elif letra2 =="h":
+        h = "f"
+        print (h)
+elif letra2 =="i":
+        i = "8"
+        print (i)
+elif letra2 =="j":
+        j = "g"
+        print (j)
+elif letra2 =="k":
+        k = "h"
+        print (k)
+elif letra2 =="m":
+        m = "m"
+        print (m)
+elif letra2 =="n":
+        n = "m"
+        print (n)
+elif letra2 =="l":
+        l = "i"
+        print (l)
+elif letra2 =="o":
+        o = "9"
+        print (o)
+elif letra2 =="p":
+        p = "0"
+        print (p)
+elif letra2 =="q":
+        q = "1"
+        print (q)
+elif letra2 =="r":
+        r = "4"
+        print (r)
+elif letra2 =="s":
+        s = "b"
+        print (s)
+elif letra2 =="t":
+        t = "5"
+        print (t)
+elif letra2 =="u":
+        u = "7"
+        print (u)
+elif letra2 =="v":
+        v = "b5"
+        print (v)
+
+print (w)
+elif letra2 =="x":
+        x = "c3"
+        print (x)
+elif letra2 =="y":
+        y = "6"
+        print (y)
+elif letra2 =="z":
+        z = "x1"
+
+
+
+
+
+if letra3 =="a":
+        a = "a"
+        print (a)
+elif letra3 =="b":
+        b = "n"
+        print (b)
+elif letra2 =="c":
+        c = "v"
+        print (c)
+elif letra3 =="d":
+        d = "c"
+        print (d)
+elif letra3 =="e":
+        e = "3"
+        print (e)
+elif letra3 =="f":
+        f = "d"
+        print (f)
+elif letra3 =="g":
+        g = "e"
+        print (g)
+elif letra3 =="h":
+        h = "f"
+        print (h)
+elif letra3 =="i":
+        i = "8"
+        print (i)
+elif letra3 =="j":
+        j = "g"
+        print (j)
+elif letra3 =="k":
+        k = "h"
+        print (k)
+elif letra3 =="m":
+        m = "m"
+        print (m)
+elif letra3 =="n":
+        n = "m"
+        print (n)
+elif letra3 =="l":
+        l = "i"
+        print (l)
+elif letra3 =="o":
+        o = "9"
+        print (o)
+elif letra3 =="p":
+        p = "0"
+        print (p)
+elif letra3 =="q":
+        q = "1"
+        print (q)
+elif letra3 =="r":
+        r = "4"
+        print (r)
+elif letra3 =="s":
+        s = "b"
+        print (s)
+elif letra3 =="t":
+        t = "5"
+        print (t)
+elif letra3 =="u":
+        u = "7"
+        print (u)
+elif letra3 =="v":
+        v = "b5"
+        print (v)
+elif letra3 =="w":
+        w = "2"
+        print (w)
+elif letra3 =="x":
+        x = "c3"
+        print (x)
+elif letra3 =="y":
+
+
+
+if letra4 =="a":
+        a = "a"
+        print (a)
+elif letra4 =="b":
+        b = "n"
+        print (b)
+elif letra4 =="c":
+        c = "v"
+        print (c)
+elif letra4 =="d":
+        d = "c"
+        print (d)
+elif letra4 =="e":
+        e = "3"
+        print (e)
+elif letra4 =="f":
+        f = "d"
+        print (f)
+elif letra4 =="g":
+        g = "e"
+        print (g)
+elif letra4 =="h":
+        h = "f"
+        print (h)
+elif letra4 =="i":
+        i = "8"
+        print (i)
+
+elif letra4 =="j":
+        j = "g"
+        print (j)
+elif letra4 =="k":
+        k = "h"
+        print (k)
+elif letra4 =="m":
+        m = "m"
+        print (m)
+elif letra4 =="n":
+        n = "m"
+        print (n)
+elif letra4 =="l":
+        l = "i"
+        print (l)
+elif letra4 =="o":
+        o = "9"
+        print (o)
+elif letra4 =="p":
+        p = "0"
+        print (p)
+elif letra4 =="q":
+        q = "1"
+        print (q)
+elif letra4 =="r":
+        r = "4"
+        print (r)
+elif letra4 =="s":
+        s = "b"
+        print (s)
+elif letra4 =="t":
+        t = "5"
+        print (t)
+elif letra4 =="u":
+        u = "7"
+        print (u)
+elif letra4 =="v":
+        v = "b5"
+        print (v)
+elif letra4 =="w":
+        w = "2"
+        print (w)
+elif letra4 =="x":
+        x = "c3"
+        print (x)
+elif letra4 =="y":
